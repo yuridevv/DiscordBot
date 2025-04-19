2555362432bc8360bd85a1fe4bf95f15a4903775
yuridevv <contatoyurifernandes@gmail.com>
2025-04-19 14:43:03 -0300

    New small features
    -prettier "help" command
    -info command added
    -server info command added
    -command description added
    -message clear command added

--- DiscordBot.py
+++ DiscordBot.py
@@ -7,7 +7,8 @@ import aiohttp
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
-client = commands.Bot(command_prefix='-', intents=intents)
+intents.presences = True
+client = commands.Bot(command_prefix='-', help_command=None, intents=intents)
 
 async def get_gif_url(query):
     api_key = "jtyizMERY5ZSUDoGW8Usq7mHKkiORRXZ"
@@ -31,20 +32,31 @@ async def on_ready():
     print(f"Bot online at {current_time}")
 
 
-@client.command()
+@client.command(help="tell you the bot latency!")
 async def ping(ctx):
     latency = client.latency * 1000
     await ctx.message.reply(f':ping_pong: Pong! **{latency:.2f}ms**')
 
 
-@client.command()
+@client.command(help="tell you what time is it based on your computer clock!")
 async def clock(ctx):
     clock = datetime.datetime.now()
     current_time = clock.strftime("%H:%M")
     await ctx.message.reply(f"It is **{current_time}** o clock :)")
 
+@client.command(help="delete past messages")
+async def clear(ctx, number: int = None):
+    if not number or not isinstance(number, int) or number <= 0:
+        await ctx.message.reply("ERROR: Number invalid")
+        return
+    elif number > 100:
+        await ctx.message.reply("ERROR: Number bigger than 100 ")
+        return
+    await ctx.channel.purge(limit=number+ 1)
+    await ctx.send(f"{number} messages cleared! {ctx.author.mention}", delete_after=20)
 
-@client.command()
+
+@client.command(help="show information about an user!")
 async def userinfo(ctx, member: discord.Member = None):
     member = member or ctx.author
     embed = discord.Embed(title="User Information:", color=discord.Color.random())
@@ -57,6 +69,40 @@ async def userinfo(ctx, member: discord.Member = None):
     embed.add_field(name="Top Role: ", value=member.top_role.name, inline=False)
     await ctx.message.reply(embed=embed)
 
+@client.command(help="show you all my commands!")
+async def help(ctx):
+    embed = discord.Embed(title=f"{client.user}'s COMMANDS", color=discord.Color.random())
+    if ctx.guild.icon:
+        embed.set_thumbnail(url=ctx.guild.icon.url)
+    embed.add_field(name='"INFO" COMMAND', value="Info about a certain command: '-info [command]'", inline=True )
+    embed.add_field(name="General:", value="-ping; -clock; -userinfo;", inline=False)
+    embed.add_field(name="Testing: ", value="-fakejoin", inline=False)
+
+    await ctx.message.reply(embed=embed)
+
+@client.command(help="tell you the command description")
+async def info(ctx, command: str = None):
+    if not command:
+        await ctx.message.reply("Write the name of the command you want to know more about!")
+        return
+    
+    command_obj = client.get_command(command)
+    if command_obj:
+        description = command_obj.help or "This command has no description yet. :("
+        await ctx.message.reply(f"**{command_obj.name}**: {description}")
+    else:
+        await ctx.message.reply(":cross_mark: Command not found, sorry :(")
+
+@client.command(help="tell information about the server")
+async def serverinfo(ctx):
+    online = len([m for m in ctx.guild.members if m.status in [discord.Status.online, discord.Status.do_not_disturb]])
+    embed = discord.Embed(title=f'"{ctx.guild.name}" INFORMATION', color=discord.Color.random())
+    if ctx.guild.icon:
+        embed.set_thumbnail(url=ctx.guild.icon.url)
+    embed.add_field(name="Date Created:", value=f"{ctx.guild.created_at.strftime("%d/%m/%Y")}",inline=True)
+    embed.add_field(name="Member Count:", value=f"{ctx.guild.member_count} members ({online} online)", inline=True)
+    embed.add_field(name="Server Owner:", value=f"{ctx.guild.owner.name} ({ctx.guild.owner.status})", inline=False)
+    await ctx.send(embed=embed)
 
 @client.event
 async def on_command(ctx, member: discord.member = None):
@@ -83,13 +129,9 @@ async def on_member_join(member):
         await welcome_message(member, channel)
 
 #Test command (fake member join)
-@client.command()
+@client.command(help="simulates a new member joining the server (for testing purposes)!")
 async def fakejoin(ctx):
     await welcome_message(ctx.author, ctx.channel)
-
-
-    
-
 
 
 client.run()
